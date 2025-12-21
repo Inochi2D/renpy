46395d4f77eafb13d8e899c3e4e6bcc68ee8ba9d
Tom Rothamel <pytom@bishoujo.us>
2025-12-20 22:51:33 -0500

    Add config.special_directory_map to map special directory names to lists of directories.

--- renpy/config.py
+++ renpy/config.py
@@ -1632,6 +1632,11 @@ tlid_only_considers_say: bool = False
 If True, only say statements will be assigned translation ids.
 """
 
+special_directory_map: dict[str, list[str]] = { 'images' : [ 'images' ], 'audio' : [ 'audio' ], 'fonts' : [ 'fonts' ] }
+"""
+This maps the special directory names ('images', 'audio', 'fonts') to a list of directories that will
+be searched for that kind of file.
+"""
 
 del os
 del collections
