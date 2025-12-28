bdd850ea843995ef2e9b3462d58d691acc76dccd
Tom Rothamel <pytom@bishoujo.us>
2025-12-27 23:26:38 -0500

    Add safe-text mode.
    This prevents a crash when text with unmatched text tags is displayed, by displaying the text
    as-is instead. The mode is enabled by default in released games, and disabled in developer mode.

--- renpy/config.py
+++ renpy/config.py
@@ -1637,12 +1637,6 @@ safe_text: bool = False
 If True, invalid text is displayed. If False, it's ignored.
 """
 
-special_directory_map: dict[str, list[str]] = { 'images' : [ 'images' ], 'audio' : [ 'audio' ], 'fonts' : [ 'fonts' ] }
-"""
-This maps the special directory names ('images', 'audio', 'fonts') to a list of directories that will
-be searched for that kind of file.
-"""
-
 
 del os
 del collections
