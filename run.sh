d4cbf81737aaa41fa2f280851d99afa01c1b6ce6
Tom Rothamel <pytom@bishoujo.us>
2026-04-14 08:30:29 -0400

    Exit 0 on builds.

--- run.sh
+++ run.sh
@@ -67,6 +67,7 @@ python "$ROOT/distribute.py" --link-directories
 
 if  [ "$1" = "--build" ] ; then
     echo "Ren'Py build complete."
+    exit 0
 else
     exec $RENPY_GDB python $ROOT/renpy.py "$@"
 fi
